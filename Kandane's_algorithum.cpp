2846c38257e26460a0a9c7c4bdd374956577a4f5
AdithyakrishnaV <adithyakr007@gmail.com>
2021-09-23 13:43:03 +0530

    Kandane's algorithum

--- Kandane's_algorithum.cpp
+++ Kandane's_algorithum.cpp
@@ -1,35 +1,35 @@
 
                 //Kandane's algorithum
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//   int n;
-//   cin>>n;
+int main(){
+  int n;
+  cin>>n;
 
-//   int a[n];
+  int a[n];
 
-//   for(int i=0; i<n; i++){
-//     cin>>a[i];
-//   }
+  for(int i=0; i<n; i++){
+    cin>>a[i];
+  }
 
-//   int max_sum = INT_MIN, current_sum=0;
+  int max_sum = INT_MIN, current_sum=0;
 
-//   for(int i=0; i<n; i++){
+  for(int i=0; i<n; i++){
     
-//     current_sum = current_sum+a[i];
+    current_sum = current_sum+a[i];
 
-//     if(current_sum > max_sum){
-//       max_sum = current_sum;
-//     }
-//     if(current_sum < 0){
-//       current_sum = 0;
-//     }
+    if(current_sum > max_sum){
+      max_sum = current_sum;
+    }
+    if(current_sum < 0){
+      current_sum = 0;
+    }
 
-//   }
+  }
 
-//   cout<<max_sum<<endl;
+  cout<<max_sum<<endl;
  
-//   return 0;
-// }
+  return 0;
+}
